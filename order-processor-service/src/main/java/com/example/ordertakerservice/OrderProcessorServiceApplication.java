dd186b4fc691b55baeb2e4c11e4d7049cc51801b
Cuong Quoc Dang <84326038+cuongquocdang@users.noreply.github.com>
2024-05-22 13:42:55 +0700

    chore(prj): init taker and processor service

---
+++ order-processor-service/src/main/java/com/example/ordertakerservice/OrderProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordertakerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderProcessorServiceApplication.class, args);
+    }
+
+}
---
+++ order-taker-service/src/main/java/com/example/ordertakerservice/OrderTakerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ordertakerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTakerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderTakerServiceApplication.class, args);
+    }
+
+}
